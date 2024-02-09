df8b4ea3fd0645b004e10ded28167691e3669075
Yao Hao <2302952@sit.singaporetech.edu.sg>
2024-02-09 20:55:47 +0800

    test

--- OOPTeamProject-desktop/src/com/mygdx/game/DesktopLauncher.java
+++ OOPTeamProject-desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
-//itworks
+//test
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
