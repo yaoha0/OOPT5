81bc2a1395478174e4c7675d24c6d4557c183bf4
k-x-18 <t.kaixuan18@gmail.com>
2024-02-21 00:51:19 +0800

    Fixed some logic for collision

--- OOPTeamProject-desktop/src/com/mygdx/game/collision/CollisionManager.java
+++ OOPTeamProject-desktop/src/com/mygdx/game/collision/CollisionManager.java
@@ -38,7 +38,7 @@ public class CollisionManager {
     }
     //Calculates the distance between object1 and object2, and returns T/F depending on collision
     public boolean isColliding(Entity obj1, Entity obj2) {
-    	float hitbox = (obj1.getWidth() + obj2.getHeight())/2;
+    	float hitbox = (obj1.getWidth() + obj2.getHeight())/3;
     	double distanceSquared = Math.pow(obj1.getX() - obj2.getX(), 2) + Math.pow(obj1.getY() - obj2.getY(), 2);
         double distance = Math.sqrt(distanceSquared);
         return distance <= hitbox;
@@ -46,17 +46,16 @@ public class CollisionManager {
     
     
     //ToBeImplemented
-    public void handleCollision(Entity obj1, Entity obj2) {	
-        // Handle collision with player
-    	if (obj1 instanceof Player && obj2 instanceof Collectible || obj1 instanceof Collectible && obj2 instanceof Player) {
-    		System.out.println("Player has collided with Collectible");
-    		entityManager.removeEntity(obj2); // Remove collectible from EntityManager
-    		Collectiblecount += 1;
-    		System.out.println("Player has collected " + Collectiblecount + " collectibles");
-        } else if (obj1 instanceof Player && obj2 instanceof Enemy || obj1 instanceof Collectible && obj2 instanceof Enemy) {
-            // Handle collision with enemy
-        	System.out.println("Player has collided with Enemy");
+    public void handleCollision(Entity obj1, Entity obj2) {
+        if ((obj1 instanceof Player && obj2 instanceof Collectible) || (obj1 instanceof Collectible && obj2 instanceof Player)) {
+            System.out.println("Player has collided with Collectible");
+            entityManager.removeEntity(obj1 instanceof Collectible ? obj1 : obj2); // Remove collectible from EntityManager
+            removeEntity(obj1 instanceof Collectible ? obj1 : obj2);
+            Collectiblecount += 1;
+            System.out.println("Player has collected " + Collectiblecount + " collectibles");
+        } else if (obj1 instanceof Player && obj2 instanceof Enemy || obj1 instanceof Enemy && obj2 instanceof Player) {
+            System.out.println("Player has collided with Enemy");
         }
-        }
+    }
     	
     }
