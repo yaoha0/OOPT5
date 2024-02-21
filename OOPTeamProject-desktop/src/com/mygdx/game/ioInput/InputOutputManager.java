1ecb4a2d4ec2ba9956ccf91593f16d7273272177
yaoha0 <143116210+yaoha0@users.noreply.github.com>
2024-02-21 16:12:55 +0800

    Add files via upload
    Shifted Input&Output Code to IOManager

--- OOPTeamProject-desktop/src/com/mygdx/game/ioInput/InputOutputManager.java
+++ OOPTeamProject-desktop/src/com/mygdx/game/ioInput/InputOutputManager.java
@@ -4,13 +4,22 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 
-import player.control.PlayerControlManager;
+import playerControl.PlayerControlManager;
+import simulationLC.PopupManager;
+import simulationLC.SimulationLifeCycle;
+import simulationLC.Ellipsis;
 
 public class InputOutputManager implements InputProcessor {
     private PlayerControlManager playerControlManager;
+    private PopupManager popupManager;
+    private Ellipsis ellipsis;
 
-    public InputOutputManager(PlayerControlManager playerControlManager) {
+
+
+    public InputOutputManager(PlayerControlManager playerControlManager, PopupManager popupManager, Ellipsis ellipsis) {
         this.playerControlManager = playerControlManager;
+        this.popupManager = popupManager;
+        this.ellipsis = ellipsis;
         // Register this class as the input processor
         Gdx.input.setInputProcessor(this);
     }
@@ -30,7 +39,14 @@ public class InputOutputManager implements InputProcessor {
                 // Make player jump
                 playerControlManager.makePlayerJump();
                 break;
-
+                
+            case Input.Keys.ESCAPE:
+            	if (popupManager.isPopupVisible()) {
+                    // Only resume if popup is visible
+                    popupManager.resumeGame();
+                    System.out.println("Game resumed."); // Resume the game
+                    break;
+                }         	
         }
         return true;
     }
@@ -54,10 +70,67 @@ public class InputOutputManager implements InputProcessor {
         // Not used but must be implemented
         return false;
     }
+    
+    public boolean isPauseButtonClicked(float x, float y) {
+    	 float buttonWidth = 50;
+         float buttonHeight = 50;
+         float buttonSpacing = 40; // Space between buttons
+         float totalButtonWidth = 2 * buttonWidth + buttonSpacing;
+         float buttonsX = (Gdx.graphics.getWidth() - totalButtonWidth) / 2f;
+         float buttonY = (Gdx.graphics.getHeight() + buttonHeight) / 2f;
+         
+         if (x > buttonsX && x < buttonsX + buttonWidth &&
+                 y > buttonY && y < buttonY + buttonHeight) {
+        	 		return true;//touch inside bound
+         }
+         return false; //touch outside bound
+    }
+    
+    public boolean isExitButtonClicked(float x, float y) {
+	   	 float buttonWidth = 50;
+	     float buttonHeight = 50;
+	     float buttonSpacing = 40; // Space between buttons
+	     float totalButtonWidth = 2 * buttonWidth + buttonSpacing;
+	     float buttonsX = (Gdx.graphics.getWidth() - totalButtonWidth) / 2f;
+	     float buttonY = (Gdx.graphics.getHeight() + buttonHeight) / 2f;
+	     float exitButtonX = buttonsX + buttonWidth + buttonSpacing;
+    	if (x > exitButtonX && x < exitButtonX + buttonWidth &&
+                y > buttonY && y < buttonY + buttonHeight) {
+            return true;//touch inside bound
+        }
+    	return false;//touch outside bound
+    }
+    
+   
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        // Not used in this context
+    	
+    	float touchX = screenX;
+        float touchY = Gdx.graphics.getHeight() - screenY;
+        
+        
+    	Gdx.app.log("TouchDown", "Touch at: " + touchX + ", " + touchY);
+    	
+    	
+        if (touchX >= ellipsis.getX() && touchX <= ellipsis.getX() + ellipsis.getWidth() &&
+                touchY >= ellipsis.getY() && touchY <= ellipsis.getY() + ellipsis.getHeight()) {
+                popupManager.togglePopupVisibility();
+                return true;
+           }
+        
+        if (popupManager.isPopupVisible()) {
+            // Assuming PopupManager has methods like isPauseButtonClicked(touchX, touchY) and isExitButtonClicked(touchX, touchY)
+            if (isPauseButtonClicked(touchX, touchY)) {
+                popupManager.toggleGamePause(); // This method should handle toggling the pause state and calling simulationLifeCycle methods as needed
+            } else if (isExitButtonClicked(touchX, touchY)) {
+                popupManager.exitGame(); // Assumes a method in PopupManager that handles game exit
+            }
+            return true; // Consume the touch event if it's within the popup
+        }
+              
+        
+        
         return false;
     }
 
