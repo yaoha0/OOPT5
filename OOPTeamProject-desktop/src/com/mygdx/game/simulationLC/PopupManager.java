c396613cbb6644c928747bae3130b503e52bcadf
yaoha0 <143116210+yaoha0@users.noreply.github.com>
2024-02-21 16:11:25 +0800

    Add files via upload
    Shifted input&output codes to IOManager

--- OOPTeamProject-desktop/src/com/mygdx/game/simulationLC/PopupManager.java
+++ OOPTeamProject-desktop/src/com/mygdx/game/simulationLC/PopupManager.java
@@ -10,7 +10,7 @@ public class PopupManager {
     private Texture pauseButtonTexture;
     private Texture exitButtonTexture;
     private Texture playButtonTexture;
-    private boolean isPopupVisible;
+    public boolean isPopupVisible;
     private boolean isPaused;
     private SimulationLifeCycle simulationLifeCycle;
 
@@ -23,56 +23,26 @@ public class PopupManager {
         exitButtonTexture = new Texture("simulationLC/exit.png");
         playButtonTexture = new Texture("simulationLC/playbtn.png");
     }
-
-    public void handleInput(Ellipsis ellipsis) {
-        // if ellipsis is clicked, show the popup with pause and exit buttons
-        if (Gdx.input.justTouched()) {
-            float touchX = Gdx.input.getX();
-            float touchY = Gdx.graphics.getHeight() - Gdx.input.getY(); // Invert Y axis
-            if (touchX > ellipsis.getX() && touchX < ellipsis.getX() + ellipsis.getWidth() &&
-                    touchY > ellipsis.getY() && touchY < ellipsis.getY() + ellipsis.getHeight()) {
-                isPopupVisible = !isPopupVisible; // Toggle the popup visibility
-                if (isPopupVisible) {
-                    //pause the game when the popup is visible
-                    simulationLifeCycle.pauseGame();
-                } else {
-                    //resume game when popup is not there
-                    simulationLifeCycle.resumeGame();
-                    isPopupVisible = false;
-                }
-            }
-
-            // If popup is visible and pause or exit button is clicked, perform the corresponding action
-            if (isPopupVisible) {
-                float buttonWidth = 50;
-                float buttonHeight = 50;
-                float buttonSpacing = 40; // Space between buttons
-                float totalButtonWidth = 2 * buttonWidth + buttonSpacing;
-                float buttonsX = (Gdx.graphics.getWidth() - totalButtonWidth) / 2f;
-                float buttonY = (Gdx.graphics.getHeight() + buttonHeight) / 2f;
-                float exitButtonX = buttonsX + buttonWidth + buttonSpacing;
-
-                //  if pause button is clicked then determine the status
-                if (touchX > buttonsX && touchX < buttonsX + buttonWidth &&
-                        touchY > buttonY && touchY < buttonY + buttonHeight) {
-                    isPaused = !isPaused; // Toggle the paused state
-                    if (isPaused) {
-                        simulationLifeCycle.pauseGame(); // pause game
-                    } else {
-                        simulationLifeCycle.resumeGame(); // resume game status
-                        isPopupVisible = false;
-                    }
-                }
-
-                //if exit button is clicked then exit game
-                if (touchX > exitButtonX && touchX < exitButtonX + buttonWidth &&
-                        touchY > buttonY && touchY < buttonY + buttonHeight) {
-                    simulationLifeCycle.exitGame();
-                }
-
-            }
+    
+    public void togglePopupVisibility() {
+    	isPopupVisible = !isPopupVisible;
+    }
+    
+    public void toggleGamePause() {
+        // Toggle the game's pause state and perform any necessary actions
+        isPaused = !isPaused;
+        if (isPaused) {
+            simulationLifeCycle.pauseGame();
+        } else {
+            simulationLifeCycle.resumeGame();
+            isPopupVisible = false;
         }
     }
+    
+    public void exitGame() {
+    	simulationLifeCycle.exitGame();
+    }
+    
 
     public void render() {
         batch.begin();
