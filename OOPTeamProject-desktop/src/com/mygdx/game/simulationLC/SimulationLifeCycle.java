73bfdf2cffc369e88e98d867b58d5978d6fa6943
vannxssaa <65593193+vannxssaa@users.noreply.github.com>
2024-02-22 15:19:43 +0800

    updated SMLC

--- OOPTeamProject-desktop/src/com/mygdx/game/simulationLC/SimulationLifeCycle.java
+++ OOPTeamProject-desktop/src/com/mygdx/game/simulationLC/SimulationLifeCycle.java
@@ -1,9 +1,21 @@
 package simulationLC;
 
+import com.mygdx.game.GameMaster;
+
 public class SimulationLifeCycle {
     private boolean isPaused;
 
     private boolean isExiting;
+
+    private GameMaster gameMaster; // Add a reference to the GameMaster
+
+    public SimulationLifeCycle(GameMaster gameMaster) {
+        this.gameMaster = gameMaster;
+    }
+
+    public void startGame() {
+        gameMaster.setScreen(gameMaster.screenManager.getMainMenuScreen());
+    }
     public void pauseGame() {
         // the pausing is done playercontrolmanager in the update function
         isPaused = true;
