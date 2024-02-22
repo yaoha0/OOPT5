8d61524fc1611aff623638c684761ad8755ca767
vannxssaa <65593193+vannxssaa@users.noreply.github.com>
2024-02-22 15:21:00 +0800

    updated desktopLauncher and GameMaster

--- OOPTeamProject-desktop/src/com/mygdx/game/DesktopLauncher.java
+++ OOPTeamProject-desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -4,13 +4,13 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
-//test
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setTitle("OOPTeamProject");
+		config.setTitle("Final_Project_Part1");
+		
 		config.setForegroundFPS(60);
-		config.setWindowedMode(800, 600);
-		new Lwjgl3Application(new GameMaster(), config);
+        config.setWindowedMode(800, 600);
+		new Lwjgl3Application(GameMaster.getInstance(), config);
 	}
 }
--- OOPTeamProject-desktop/src/com/mygdx/game/GameMaster.java
+++ OOPTeamProject-desktop/src/com/mygdx/game/GameMaster.java
@@ -1,15 +1,18 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.Game;
+
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 import scene.EndScreen;
 import scene.PlayScreen;
 import scene.ScreenManager;
 import simulationLC.SimulationLifeCycle;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
 public class GameMaster extends Game {
+	private static GameMaster instance = null;
 	
 	public SpriteBatch batch;
 	public ShapeRenderer shape;
@@ -18,14 +21,27 @@ public class GameMaster extends Game {
 	public EndScreen endScreen;
 	public SimulationLifeCycle simulationLifeCycle;
 	
-    @Override
-    public void create() {
-    	batch = new SpriteBatch();
-    	shape = new ShapeRenderer();
-    	screenManager = ScreenManager.getInstance();
-    	screenManager.initialize(this);
-    	screenManager.showMainScreen();
-      	playScreen = new PlayScreen(batch);
-    	endScreen = new EndScreen(batch, simulationLifeCycle);
-    }
+	private GameMaster() {}
+
+	public static GameMaster getInstance() {
+		if (instance == null) {
+			instance = new GameMaster();
+		}
+		return instance;
+	}
+	
+	@Override
+	public void create() {
+	    batch = new SpriteBatch();
+	    shape = new ShapeRenderer();
+	    simulationLifeCycle = new SimulationLifeCycle(this); // Pass 'this' as the GameMaster instance
+	    screenManager = ScreenManager.getInstance();
+	    screenManager.initialize(this, batch, simulationLifeCycle); // Make sure MainMenuScreen is initialized inside this method
+	    playScreen = new PlayScreen(batch);
+	    endScreen = new EndScreen(batch, simulationLifeCycle);
+	    // Now that everything is initialized, show the main screen.
+	    screenManager.showMainScreen();
+	}
+    
+    
 }
