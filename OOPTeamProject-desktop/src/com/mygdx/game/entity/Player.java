cffbd194f4949b2bfa4946a6b502803db20ab466
vannxssaa <65593193+vannxssaa@users.noreply.github.com>
2024-03-13 15:41:59 +0800

    edited player
    added new attributes
    added getters and setters
    added methods for input

--- OOPTeamProject-desktop/src/com/mygdx/game/entity/Player.java
+++ OOPTeamProject-desktop/src/com/mygdx/game/entity/Player.java
@@ -6,11 +6,13 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Player extends Entity {
-    private float velocityY = 0;
+    private float velocityY, velocityX = 0;
+    private float lastSafeX, lastSafeY;
     private boolean isOnGround;
-    private boolean isFacingLeft = false;
     private AnimationHandler animationHandler;
-    private boolean isWalking, isJumping = false;
+    private boolean isWalking, isJumping, isFacingLeft, hasFallen = false;
+    public static final float PIT_LEVEL = -300;
+
 
     // Method to retrieve the AnimationHandler instance
     public AnimationHandler getAnimationHandler() {
@@ -19,7 +21,8 @@ public class Player extends Entity {
 
     public Player(String idleTexturePath, String walkTexturePath, String jumpTexturePath, int idleFrames, int walkFrames, int jumpFrames, float x, float y, float width, float height) {
         super(idleTexturePath, x, y, width, height);
-
+        lastSafeX = this.x;
+        lastSafeY = this.y;
         // idle, walk, jump, idle frames, walk frames, jump frames,
         animationHandler = new AnimationHandler(idleTexturePath, walkTexturePath, jumpTexturePath, idleFrames, walkFrames, jumpFrames);
     }
@@ -40,35 +43,63 @@ public class Player extends Entity {
     
     @Override
     public void update(float deltaTime) {
-        // Update logic, including simple gravity, should update this.x and this.y from the Entity class
+        System.out.println("Player Y Axis: " + this.y);
+        // If the player has reached the pit level, reset to the last safe position
+        if (this.getHasFallen() && this.y <= PIT_LEVEL) {
+            // Reset the player's position to the last safe position
+            this.x = lastSafeX;
+            this.y = lastSafeY;
+            this.velocityY = 0; // Reset the velocity
+            this.hasFallen = false; // Player has finished falling
+            // Depending on your game, you might want to set isOnGround based on whether the last safe position was on the ground
+            this.isOnGround = true;
+            updateLastSafePosition();
+        }
+    }
 
-        if (!isOnGround) {
-            velocityY -= 9.8 * deltaTime; // Gravity effect
-            this.y += velocityY * deltaTime; // Apply movement to this.y of Entity
+    // Call this when the player falls through a hole
+    public void fall() {
+        this.hasFallen = true;
+        this.isOnGround = false; // The player is no longer on the ground
+        //this.velocityY = 0; // Reset the velocity as the fall starts
+    }
 
-            if (this.y <= 0) { // Simple ground collision detection
-                this.y = 0;
-                isOnGround = true;
-                velocityY = 0;
-            }
+    public void land(float platformTopY) {
+        this.setY(platformTopY-50); // Set the bottom edge of the player to the top of the platform
+        this.setVelocityY(0);    // Stop the vertical movement
+        this.setIsOnGround(true);
+        this.setIsJumping(false);
+    }
+
+    // Method to update the last safe position
+    public void updateLastSafePosition() {
+        if (this.isOnGround && !this.hasFallen) {
+            lastSafeX = this.x;
+            lastSafeY = this.y;
         }
     }
 
     
-    // Getters and setters for the Player class should refer to the x and y of the Entity class
-    public float getX() { return this.x; }
-    public float getY() { return this.y; }
+    // Getters
+
+
     public float getVelocityY() { return velocityY; }
     public boolean getIsOnGround() { return isOnGround; }
     public boolean getIsWalking () { return isWalking; }
     public boolean getIsJumping () { return isJumping; }
     public boolean getIsFacingLeft () { return isFacingLeft; }
-    
-    public void setX(float x) { this.x = x; }
-    public void setY(float y) { this.y = y; }
+    public boolean getHasFallen() {return hasFallen;}
+    public float getLastSafeX() { return lastSafeX; }
+    public float getLastSafeY() { return lastSafeY; }
+
+    //Setters
     public void setVelocityY(float velocityY) { this.velocityY = velocityY; }
-    public void setOnGround(boolean onGround) { isOnGround = onGround; }
-    public void setIsWalking(boolean walking) { isWalking = walking; }
-    public void setIsJumping(boolean jumping) { isJumping = jumping; }
-    public void setFacingLeft(boolean facingLeft) { isFacingLeft = facingLeft; }
+    public void setIsOnGround(boolean onGround) { this.isOnGround = onGround; }
+    public void setIsWalking(boolean walking) { this.isWalking = walking; }
+    public void setIsJumping(boolean jumping) { this.isJumping = jumping; }
+    public void setFacingLeft(boolean facingLeft) { this.isFacingLeft = facingLeft; }
+    public void setHasFallen(boolean hasFallen) { this.hasFallen = hasFallen; }
+    public void setLastSafeY(float lastSafeY) { this.lastSafeY = lastSafeY; }
+    public void setLastSafeX(float lastSafeX) { this.lastSafeX = lastSafeX; }
+
 }
