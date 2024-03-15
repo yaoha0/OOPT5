bd9259bd72ca9b6de705d7477646f957b7531140
k-x-18 <146832253+k-x-18@users.noreply.github.com>
2024-03-15 15:15:09 +0800

    Update readability

--- OOPTeamProject-desktop/src/com/mygdx/game/aiControl/DecisionMaking.java
+++ OOPTeamProject-desktop/src/com/mygdx/game/aiControl/DecisionMaking.java
@@ -6,12 +6,15 @@ public class DecisionMaking {
 
     private DetectionSystem detectionSystem;
     private PathfindingSystem pathfindingSystem;
+    private static final int LEFT_BOUNDARY = -20;
+    private static final int RIGHT_BOUNDARY = 690;
+    private static final int GRAVITY = 1;
 
     public DecisionMaking(DetectionSystem detectionSystem, PathfindingSystem pathfindingSystem) {
         this.detectionSystem = detectionSystem;
         this.pathfindingSystem = pathfindingSystem;
     }
-
+    //Makes AI Attack player if within detection range, and idle if no players within range
     public void makeDecision(Entity ai, Entity player, int detectionRange, int speed) {
         if (detectionSystem.playerDetected(ai, player, detectionRange)) {
             attackTarget(ai, player, speed);
@@ -26,14 +29,14 @@ public class DecisionMaking {
 
     //Makes the AI follow target, and ensures it does not go out of the game area
     public void attackTarget(Entity ai, Entity target,int speed) {
-    	if (target.getX() < ai.getX() && ai.getX() > -20) {
+    	if (target.getX() < ai.getX() && ai.getX() > LEFT_BOUNDARY) {
     		ai.setX(ai.getX() - speed);
     	}
-    	if (target.getX() > ai.getX() && ai.getX() < 690) { 
+    	if (target.getX() > ai.getX() && ai.getX() < RIGHT_BOUNDARY) {
     		ai.setX(ai.getX() + speed);
     	}
-        if (ai.getY() > 0){            //applies gravity to AI
-            ai.setY(ai.getY() - speed);
+        if (ai.getY() > 0){
+            ai.setY(ai.getY() - GRAVITY);
         }
     }
 
