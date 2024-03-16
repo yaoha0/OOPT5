a52a270a57aa5ce1f3f91cdcdbc15cd89a3b2d3f
vannxssaa <65593193+vannxssaa@users.noreply.github.com>
2024-03-17 00:28:24 +0800

    updated ioinputmanager
    - cleaned up code

--- OOPTeamProject-desktop/src/com/mygdx/game/ioInput/InputOutputManager.java
+++ OOPTeamProject-desktop/src/com/mygdx/game/ioInput/InputOutputManager.java
@@ -1,141 +1,190 @@
-package collision;
+package ioInput;
 
-import entity.*;
-import com.badlogic.gdx.math.Rectangle;
-import scene.ScreenManager;
-import com.badlogic.gdx.utils.Array;
-import java.util.ArrayList;
-import ioInput.InputOutputManager;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 
-public class CollisionManager {
-    private ScreenManager screenManager;
-    private int collectibleCount;
-    private ArrayList<Float> holePositions;
-    private ArrayList<Platform> platforms; // List of all platform tiles
-    private InputOutputManager inputOutputManager;
+import com.badlogic.gdx.math.MathUtils;
+import entity.Player;
+import playerControl.PlayerControlManager;
+import simulationLC.PopupManager;
+import simulationLC.Ellipsis;
 
-    public CollisionManager(ScreenManager screenManager, ArrayList<Float> holePositions, ArrayList<Platform> platforms, InputOutputManager inputOutputManager) {
-        this.screenManager = screenManager;
-        this.collectibleCount = 0;
-        this.holePositions = holePositions;
-        this.platforms = platforms;
-        this.inputOutputManager = inputOutputManager;
+public class InputOutputManager implements InputProcessor {
+    private PlayerControlManager playerControlManager;
+    private Player player;
+    private PopupManager popupManager;
+    private Ellipsis ellipsis;
+
+    public InputOutputManager(Player player ,PlayerControlManager playerControlManager, PopupManager popupManager, Ellipsis ellipsis) {
+        this.playerControlManager = playerControlManager;
+        this.popupManager = popupManager;
+        this.ellipsis = ellipsis;
+        this.player = player;
+        // Register this class as the input processor
+        Gdx.input.setInputProcessor(this);
     }
-    
-    public int getCollectibleCount() {
-        return collectibleCount;
-    }
 
-    public void updateCollisions() {
-        EntityManager entityManager = EntityManager.getInstance();
-        Array<Entity> entities = entityManager.getEntities();
-        Entity player = null;
-
-        // Find the player entity
-        for (Entity entity : entities) {
-            if (entity instanceof Player) {
-                player = entity;
+    @Override
+    public boolean keyDown(int keycode) {
+        switch (keycode) {
+            case Input.Keys.LEFT:
+                System.out.println("keyDown : Left");
+                playerControlManager.setMovingLeft(true);
                 break;
-            }
-        }
-
-        for (int i = 0; i < entities.size; i++) {
-            for (int j = i + 1; j < entities.size; j++) {
-                if (isColliding(entities.get(i), entities.get(j))) {
-                    handleCollision(entities.get(j), entities.get(i));
+            case Input.Keys.RIGHT:
+                System.out.println("keyDown : RIGHT");
+                playerControlManager.setMovingRight(true);
+                break;
+            case Input.Keys.SPACE:
+                System.out.println("Jump Pressed");
+                playerControlManager.makePlayerJump();
+                break;
+            case Input.Keys.ESCAPE:
+                if (popupManager.isPopupVisible()) {
+                    // Only resume if PopUp is visible
+                    popupManager.resumeGame();
+                    System.out.println("Game resumed."); // Resume the game
+                    break;
                 }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        switch (keycode) {
+            case Input.Keys.LEFT:
+                playerControlManager.setMovingLeft(false);
+                break;
+            case Input.Keys.RIGHT:
+                playerControlManager.setMovingRight(false);
+                break;
+            case Input.Keys.SPACE:
+                playerControlManager.onJumpKeyReleased(); // Handle jump key release
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        // Not used but must be implemented
+        return false;
+    }
+
+    public boolean isPauseButtonClicked(float x, float y) {
+        float buttonWidth = 50;
+        float buttonHeight = 50;
+        float buttonSpacing = 40; // Space between buttons
+        float totalButtonWidth = 2 * buttonWidth + buttonSpacing;
+        float buttonsX = (Gdx.graphics.getWidth() - totalButtonWidth) / 2f;
+        float buttonY = (Gdx.graphics.getHeight() + buttonHeight) / 2f;
+
+        if (x > buttonsX && x < buttonsX + buttonWidth &&
+                y > buttonY && y < buttonY + buttonHeight) {
+            return true;//touch inside bound
+        }
+        return false; //touch outside bound
+    }
+
+    public boolean isExitButtonClicked(float x, float y) {
+        float buttonWidth = 50;
+        float buttonHeight = 50;
+        float buttonSpacing = 40; // Space between buttons
+        float totalButtonWidth = 2 * buttonWidth + buttonSpacing;
+        float buttonsX = (Gdx.graphics.getWidth() - totalButtonWidth) / 2f;
+        float buttonY = (Gdx.graphics.getHeight() + buttonHeight) / 2f;
+        float exitButtonX = buttonsX + buttonWidth + buttonSpacing;
+        if (x > exitButtonX && x < exitButtonX + buttonWidth &&
+                y > buttonY && y < buttonY + buttonHeight) {
+            return true;//touch inside bound
+        }
+        return false;//touch outside bound
+    }
+
+
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+
+        float touchX = screenX;
+        float touchY = Gdx.graphics.getHeight() - screenY;
+
+
+        Gdx.app.log("TouchDown", "Touch at: " + touchX + ", " + touchY);
+
+
+        if (touchX >= ellipsis.getX() && touchX <= ellipsis.getX() + ellipsis.getWidth() &&
+                touchY >= ellipsis.getY() && touchY <= ellipsis.getY() + ellipsis.getHeight()) {
+            popupManager.togglePopupVisibility();
+            return true;
+        }
+
+        if (popupManager.isPopupVisible()) {
+            // Assuming PopupManager has methods like isPauseButtonClicked(touchX, touchY) and isExitButtonClicked(touchX, touchY)
+            if (isPauseButtonClicked(touchX, touchY)) {
+                popupManager.toggleGamePause(); // This method should handle toggling the pause state and calling simulationLifeCycle methods as needed
+            } else if (isExitButtonClicked(touchX, touchY)) {
+                popupManager.exitGame(); // Assumes a method in PopupManager that handles game exit
+            }
+            return true; // Consume the touch event if it's within the PopUp
+        }
+
+
+
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        // Not used
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        // Not used
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        // Not used
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        // Not used
+        return false;
+    }
+
+    @Override
+    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+
+    /*public void update(float deltaTime) {
+        if (player.getIsJumping()) {
+            // Apply gravity to vertical velocity
+            player.setVelocityY(GRAVITY * deltaTime );
+
+            // Limit maximum falling speed
+            player.setVelocityY(MathUtils.clamp(player.getVelocityY(),
+                    -MAX_FALL_SPEED, MAX_FALL_SPEED));
+
+
+            player.setY(player.getY() + player.getVelocityY() * deltaTime);
+
+            // Check if player has landed
+            if (player.getY() <= 0) {
+                player.setY(0); // Set player on the ground
+                player.setIsJumping(false); // Reset jump state
             }
         }
+    }*/
 
-        if (player != null) {
-            for (Platform platform : platforms) {
-                if (isEntityColliding(player, platform)) {
-                    Player p = (Player) player;
-                    handlePlatformCollision(p, platform);
-                    if (p.getVelocityY() < 0 && p.getY() > platform.getY() + platform.getHeight()) {
-                        // Correctly position the player on top of the platform
-                        p.land(platform.getY() + platform.getHeight());
-                        break; // Stop checking for more collisions
-                    }
-                    break; // Assume one collision at a time for simplicity
-                }
-            }
-            checkPlayerFall(player); // Check for falling off the platforms
-        }
-    }
 
-    public boolean isEntityColliding(Entity obj1, Entity obj2) {
-        Rectangle rect1 = new Rectangle(obj1.getX(), obj1.getY(), obj1.getWidth(), obj1.getHeight());
-        Rectangle rect2 = new Rectangle(obj2.getX(), obj2.getY(), obj2.getWidth(), obj2.getHeight());
-
-        System.out.println("Overlaps: " + rect1.overlaps(rect2));
-        return rect1.overlaps(rect2);
-    }
-
-    public boolean isColliding(Entity obj1, Entity obj2) {
-        float hitbox = (obj1.getWidth() + obj2.getHeight()) / 3;
-        double distanceSquared = Math.pow(obj1.getX() - obj2.getX(), 2) + Math.pow(obj1.getY() - obj2.getY(), 2);
-        double distance = Math.sqrt(distanceSquared);
-        return distance <= hitbox;
-
-    }
-    
-    public void handleCollision(Entity obj1, Entity obj2) {
-        if ((obj1 instanceof Player && obj2 instanceof Collectible) || (obj1 instanceof Collectible && obj2 instanceof Player)) {
-            EntityManager.getInstance().removeEntity(obj1 instanceof Collectible ? obj1 : obj2); // Remove collectible from EntityManager
-            collectibleCount += 1;
-            inputOutputManager.playCollectSound();
-            //System.out.println("Player has collected " + collectibleCount + " collectibles");
-        } else if (obj1 instanceof Player && obj2 instanceof Enemy || obj1 instanceof Enemy && obj2 instanceof Player) {
-        	inputOutputManager.playGameOverSound();
-        	screenManager.showEndScreen();  
-        }
-    }
-
-    // Method to handle platform collisions
-    private void handlePlatformCollision(Player player, Platform platform) {
-        // Get the rectangle for the platform's top surface where the player should land
-        Rectangle platformTopRect = new Rectangle(
-                platform.getX(), platform.getY() + platform.getHeight() - 1,
-                platform.getWidth(), 2 // Small thickness to represent the top surface
-        );
-
-        // Get the player's rectangle considering its current movement and position
-        Rectangle playerRect = new Rectangle(
-                player.getX(), player.getY() + player.getVelocityY(),
-                player.getWidth(), player.getHeight()
-        );
-
-        // Check if the player is falling and their bottom is colliding with the top surface of the platform
-        if (player.getVelocityY() < 0 && playerRect.overlaps(platformTopRect)) {
-            player.land(platform.getY() + platform.getHeight());
-        }
-    }
-
-    private void checkPlayerFall(Entity player) {
-        boolean isPlayerOnGround = true; // Initially assume the player is on the ground
-
-        // Assuming player's Y position at 0 means they are on the ground level
-        if (player.getY() > 0) {
-            isPlayerOnGround = false;
-        } else {
-            // Check if player is over a hole
-            for (Float holeX : holePositions) {
-                float playerMiddleX = player.getX() + player.getWidth() / 2;
-
-                if (playerMiddleX > holeX && playerMiddleX < holeX + 50) {
-                    // Player is within the horizontal bounds of a hole
-                    //System.out.println("Player has fallen.");
-                    //System.out.println("Player X: " + player.getX() + " Hole X: " + holeX);
-                    ((Player)player).fall();
-                    isPlayerOnGround = false;
-                    break; // No need to check other holes
-                }
-            }
-        }
-
-        if (isPlayerOnGround) {
-            //System.out.println("Player is on the ground.");
-        }
-    }
 }
