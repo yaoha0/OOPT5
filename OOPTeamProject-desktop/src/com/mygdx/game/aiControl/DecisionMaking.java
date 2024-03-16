a5c74f93ce8f85d03c2e533f6789553e78c7cb2e
vannxssaa <65593193+vannxssaa@users.noreply.github.com>
2024-03-17 00:34:44 +0800

    updated aicontrolmanager
    - fixed boundary code cause of it being a fixed value, so now its dynamically set to whatever the width of the screen will be

--- OOPTeamProject-desktop/src/com/mygdx/game/aiControl/DecisionMaking.java
+++ OOPTeamProject-desktop/src/com/mygdx/game/aiControl/DecisionMaking.java
@@ -1,11 +1,14 @@
 package aiControl;
 
+import com.badlogic.gdx.Gdx;
 import entity.Entity;
 
 public class DecisionMaking {
 
     private DetectionSystem detectionSystem;
     private PathfindingSystem pathfindingSystem;
+    private static final int LEFT_BOUNDARY = 0;
+    private static final int RIGHT_BOUNDARY = Gdx.graphics.getWidth();
     private static final int GRAVITY = 1;
 
     public DecisionMaking(DetectionSystem detectionSystem, PathfindingSystem pathfindingSystem) {
@@ -16,12 +19,12 @@ public class DecisionMaking {
     public void makeDecision(Entity ai, Entity player, int detectionRange, int speed) {
         if (detectionSystem.playerDetected(ai, player, detectionRange)) {
             attackTarget(ai, player, speed);
-        } 
+        }
         else {
-        	//10% chance to begin idle movement
-        	if (Math.random() < 0.1) {
-            pathfindingSystem.IdleMovement(ai, speed);
-        	}
+            //10% chance to begin idle movement
+            if (Math.random() < 0.1) {
+                pathfindingSystem.IdleMovement(ai, speed);
+            }
         }
         if (ai.getY() > 0){ // applies gravity to AI
             ai.setY(ai.getY() - GRAVITY);
@@ -30,12 +33,12 @@ public class DecisionMaking {
 
     //Makes the AI follow target, and ensures it does not go out of the game area
     public void attackTarget(Entity ai, Entity target,int speed) {
-    	if (target.getX() < ai.getX()) {
-    		ai.setX(ai.getX() - speed);
-    	}
-    	if (target.getX() > ai.getX()) {
-    		ai.setX(ai.getX() + speed);
-    	}
+        if (target.getX() < ai.getX() && ai.getX() > LEFT_BOUNDARY) {
+            ai.setX(ai.getX() - speed);
+        }
+        if (target.getX() > ai.getX() && ai.getX() < RIGHT_BOUNDARY) {
+            ai.setX(ai.getX() + speed);
+        }
     }
 
 }
