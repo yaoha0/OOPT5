fb7409d71ba4ccdf31b6147b60276d82f099c648
Hexqne <53991762+Hexqne@users.noreply.github.com>
2024-03-20 20:38:32 +0800

    Update DesktopLauncher.java

--- OOPTeamProject-desktop/src/com/mygdx/game/DesktopLauncher.java
+++ OOPTeamProject-desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -5,12 +5,36 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
+	
+	private static int width = 800;
+	private static int height = 600;
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setTitle("Final_Project_Part1");
+		config.setTitle("Final_Project");
 		
 		config.setForegroundFPS(60);
-        config.setWindowedMode(1280, 920);
+        config.setWindowedMode(width, height);
 		new Lwjgl3Application(GameMaster.getInstance(), config);
+	}
+	public static int getWidth() {
+		return width;
+	}
+	/**
+	 * @param width the width to set
+	 */
+	public static void setWidth(int width) {
+		DesktopLauncher.width = width;
+	}
+	/**
+	 * @return the height
+	 */
+	public static int getHeight() {
+		return height;
+	}
+	/**
+	 * @param height the height to set
+	 */
+	public static void setHeight(int height) {
+		DesktopLauncher.height = height;
 	}
 }
