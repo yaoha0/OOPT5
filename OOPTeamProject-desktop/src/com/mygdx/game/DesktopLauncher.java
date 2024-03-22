5ddb63b048e54c14c1d462b72d7dd425b5aa17bf
Hexqne <53991762+Hexqne@users.noreply.github.com>
2024-03-22 21:42:06 +0800

    Update DesktopLauncher.java. Set to 1600x900, disabled resize

--- OOPTeamProject-desktop/src/com/mygdx/game/DesktopLauncher.java
+++ OOPTeamProject-desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -6,14 +6,15 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	
-	private static int width = 800;
-	private static int height = 600;
+	private static int width = 1600;
+	private static int height = 900;
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setTitle("Final_Project");
 		
 		config.setForegroundFPS(60);
-        config.setWindowedMode(width, height);
+       		config.setWindowedMode(width, height);
+		config.setResizable(false);
 		new Lwjgl3Application(GameMaster.getInstance(), config);
 	}
 	public static int getWidth() {
